5d1464fe0390d32f9565c4a13aed4e9f78920198
Sergey Zavyalov <ser500vms@gmail.com>
2023-07-05 13:29:34 +0300

    Done task_4 and update readme.md

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №4:
+// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// -4 -> "4, 3, 2, 1, 0, -1, -2, -3, -4"*
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 0)
+{
+    for (int i = -number; i <= number; i++)
+Console.Write(i + ", ");
+}
+else
+{
+    for (int i = -1 * number; i >= number; i--)
+Console.Write(i + ", ");
+}
+
--- readme.md
+++ readme.md
@@ -67,3 +67,23 @@ else
 {
     Console.Write($"{numberA} не является квадратом числа {numberB} ");
 }
+
+// Задача №4:
+// Напишите программу, которая на вход принимает одно число (N), 
+// а на выходе показывает все целые числа в промежутке от -N до N.
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// -4 -> "4, 3, 2, 1, 0, -1, -2, -3, -4"*
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 0)
+{
+    for (int i = -number; i <= number; i++)
+Console.Write(i + ", ");
+}
+else
+{
+    for (int i = -1 * number; i >= number; i--)
+Console.Write(i + ", ");
+}
